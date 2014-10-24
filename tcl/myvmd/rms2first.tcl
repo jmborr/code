d08d331dd7ad35e0cdf81e55727943061105fa4b
Jose Borreguero <borreguero@gmail.com>
2014-10-24 09:58:07 -0400

    Add vmd script to align trajectory frames by RMSD

---
+++ tcl/myvmd/rms2first.tcl
@@ -0,0 +1,48 @@
+# Fittrajectory to selected frame by RMSD transformation
+
+# Call the RMSD trajectory tool
+::rmsdtt::rmsdtt
+
+######### Here set all variables
+
+set firstframe 5002
+
+#  frame to align the other frames to
+set refframe $firstframe
+
+#  write alined trajectory starting at this frame 
+set begframe $firstframe
+
+#  input and output files
+set pdbfile "8styrene32.pdb"
+set indcdfile "equil.dcd"
+set outdcdfile "junk.dcd"
+
+#  select all atoms
+$::rmsdtt::w.top.left.sel delete 1.0 end
+$::rmsdtt::w.top.left.sel insert end "all"
+
+# Set modifiers
+set ::rmsdtt::bb_only 0
+set ::rmsdtt::trace_only 0
+set ::rmsdtt::noh 0
+
+# Set reference
+set ::rmsdtt::rmsd_base top
+set ::rmsdtt::traj_ref $firstframe
+set ::rmsdtt::traj_sw 1
+
+######### Here perform all transformations
+
+# Load the trajectory
+mol new $pdbfile
+animate read dcd $indcdfile beg 0 end -1 waitfor all
+
+# Align
+::rmsdtt::doAlign
+
+# Write trajectory
+animate write dcd $outdcdfile beg $begframe end -1 waitfor all
+
+#exit script
+quit
