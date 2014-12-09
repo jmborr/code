6821088e737a9c779ba6d0ee5549b4136e245920
Jose Borreguero <borreguero@gmail.com>
2014-12-09 16:33:27 -0500

    Reduce number of building cores in mantidNightly script

--- bin/mantidNightly.sh
+++ bin/mantidNightly.sh
@@ -6,7 +6,7 @@ git stash
 git checkout master
 git fetch -p
 git pull --rebase
-cmakeMantid.py --builddir release --ncore 4 --popmsg no
-cmakeMantid.py --builddir release --ncore 4 --target doc --popmsg no
+cmakeMantid.py --builddir release --ncore 1 --popmsg no
+cmakeMantid.py --builddir release --ncore 1 --target doc --popmsg no
 git checkout -
 git stash apply
