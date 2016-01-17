725465e250e01cc894954870777380f635cfd056
Jose Borreguero <borreguero@gmail.com>
2016-01-17 13:21:33 -0500

    minor update

--- python/amber/amber14/cpptraj/command.py
+++ python/amber/amber14/cpptraj/command.py
@@ -1,13 +1,17 @@
 import os
 from tempfile import mkstemp
+from pdb import set_trace as tr
 
 def exec_cpptraj(topfile, script, delscriptfile=True):
     handle, scriptfile = mkstemp(prefix='junk', dir='/tmp')
+    handle, stdoutputfile = mkstemp(prefix='junk', dir='/tmp')
     open(scriptfile,'w').write(script)
     if topfile:
-        os.system('module load amber/amber14; cpptraj -p {0} -i {1}'.format(topfile, scriptfile))
+        os.system('module load amber/amber14; cpptraj -p {0} -i {1} > {2}'.format(topfile, scriptfile,stdoutputfile))
     else:
-        os.system('module load amber/amber14; cpptraj -i {0}'.format(scriptfile)) 
+        os.system('module load amber/amber14; cpptraj -i {0} > {2}'.format(scriptfile,stdoutputfile)) 
     if delscriptfile:
         os.system('/bin/rm {0}'.format(scriptfile))
-    return scriptfile
+    stdoutput = open(stdoutputfile).readlines()
+    os.system('/bin/rm {0}'.format(stdoutputfile))
+    return scriptfile, stdoutput
