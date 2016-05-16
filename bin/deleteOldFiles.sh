99b6dba51c54e9cc0c91870b3268acd6709bb9c4
Jose Borreguero <borreguero@gmail.com>
2016-05-16 12:32:46 -0400

    Correct mistake for deleting old files

--- bin/deleteOldFiles.sh
+++ bin/deleteOldFiles.sh
@@ -2,6 +2,4 @@
 
 # delete files older than 15 days
 find $HOME/jbq/Downloads/* -mtime +15 -exec rm -rf {} \;
-find $HOME/jbq/Downloads/.* -mtime +15 -exec rm -rf {} \;
 find /tmp/*  -mtime +15 -exec rm -rf {} \;
-find /tmp/.*  -mtime +15 -exec rm -rf {} \;
